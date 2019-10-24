ab438efdbc7d939e316b4752bbaaa5dd743d06cb
Paps90 <barra90@gmail.com>
2019-10-24 18:38:32 +0200

    Update Lesson 5

--- 5_lesson5/cnn.py
+++ 5_lesson5/cnn.py
@@ -28,8 +28,8 @@ classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+#classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+#classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
